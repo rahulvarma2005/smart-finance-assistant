060a6fc2620cd70406973e47ae2d6653d726273e
Rahul <rahulmudunuri12@gmail.com>
2025-07-27 19:02:40 +0530

    Initial commit

---
+++ src/main/java/com/financeapp/personal/smart_finance_assistant/SmartFinanceAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.financeapp.personal.smart_finance_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFinanceAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFinanceAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financeapp/personal/smart_finance_assistant/SmartFinanceAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeapp.personal.smart_finance_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinanceAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
